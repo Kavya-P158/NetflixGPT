0718cc99eea818824481f290fd555ecda786f6d8
Kavya <kavya.ponnusamy@gmail.com>
2024-07-03 10:24:20 +0530

    Updated UI

--- src/Components/VideoTitle.js
+++ src/Components/VideoTitle.js
@@ -7,7 +7,7 @@ const VideoTitle = ({ title, description }) => {
             <h1 className='text-white text-2xl md:text-5xl font-bold'>{title}</h1>
             <p className='text-white py-6 text-lg w-1/4 hidden md:inline-block'>{description}</p>
             <div className='my-4 md:mx-0'>
-                <button className='bg-white text-black  py-1 md:py-4 px-3 md:px-12 text-xl rounded-md hover:bg-opacity-40'>▶ Play</button>
+                {/* <button className='bg-white text-black  py-1 md:py-4 px-3 md:px-12 text-xl rounded-md hover:bg-opacity-40'>▶ Play</button> */}
 
                 <button className='hidden bg-gray-500 text-black p-4 px-14 mx-2 text-xl rounded-md'> ℹ More info</button>
             </div>
